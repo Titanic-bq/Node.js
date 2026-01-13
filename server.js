c23de1c296275e05ceba5d17c9646ef2616c20e4
hendrik <hendrik>
2026-01-13 13:09:46 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import http from "http";
+const PORT = 8000;
+const server = http.createServer((req, res) => {
+  res.writeHead("200", { "Content-Type": "text/html" });
+  res.end("<h1> Hello World </h1>");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
