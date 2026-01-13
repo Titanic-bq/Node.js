777594a41c01ae8697f13bc32adcbe3952e88e35
hendrik <hendrik>
2026-01-13 22:22:39 +0200

    Req

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import http from "http";
-const PORT = 8000;
+const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log(req.method);
   res.writeHead("200", { "Content-Type": "text/html" });
   res.end("<h1> Hello World </h1>");
 });
 
+appendFile.post("/users", () => {});
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
