7682060b4e1c9c7e2d4c7539c68c7c87fb3a9038
hendrik <hendrik>
2026-01-13 23:10:39 +0200

    Failide laadimine

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
-  res.writeHead("200", { "Content-Type": "text/html" });
-  res.end("<h1> Hello World </h1>");
-});
 
-appendFile.post("/users", () => {});
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname, __filename);
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filepath;
+      if (req.url === "/") {
+        filepath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filepath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("Not Found");
+      }
+      const data = await fs.readFile(filepath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead("500", { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
+});
+/*appendFile.post("/users", () => {});*/
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
