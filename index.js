fd445724d791dfb96e9c3e354e10bb13fc7279e1
hendrik <hendrik>
2026-01-13 13:09:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import { getPosts } from "./postController.js";
+
+console.log(getPosts());
+
+/*console.log("Hello, World!");
+console.log("This is the index.js file.");
+
+const generateRandomNumber = require("./utils").generateRandomNumber;
+console.log("Random Number:", generateRandomNumber());*/
