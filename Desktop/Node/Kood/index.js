8b55faaaa3e953e8cd926a9e279f816323881dae
hendrik <hendrik>
2026-01-13 11:58:58 +0200

    random number

--- Desktop/Node/Kood/index.js
+++ Desktop/Node/Kood/index.js
@@ -1,2 +1,5 @@
 console.log("Hello, World!");
 console.log("This is the index.js file.");
+
+const generateRandomNumber = require("./utils").generateRandomNumber;
+console.log("Random Number:", generateRandomNumber());
