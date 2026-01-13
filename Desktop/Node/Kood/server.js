387b91e549bfc326b960f54eaef25d75e97a5553
hendrik <hendrik>
2026-01-13 12:24:46 +0200

    server.p2

--- Desktop/Node/Kood/server.js
+++ Desktop/Node/Kood/server.js
@@ -1,9 +1,8 @@
 import http from "http";
 const PORT = 8000;
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello, World!</h1>");
-  res.statusCode = 404;
+  res.writeHead("500", { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Internal Server Error" }));
 });
 
 server.listen(PORT, () => {
