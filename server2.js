1b28aa83260c4defcf3b5fdf0055ab621e23fd24
hendrik <hendrik>
2026-01-18 16:32:06 +0200

    POST

--- server2.js
+++ server2.js
@@ -51,6 +51,23 @@ const notFoundHandler = (req, res) => {
   res.end();
 };
 
+// Route handler for POST /api/users
+const createUserHandler = (req, res) => {
+  let body = "";
+  //Listen for data
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", () => {
+    const newUser = JSON.parse(body);
+    users.push(newUser);
+    res.statusCode = 201;
+    res.write(JSON.stringify(newUser));
+    res.end();
+  });
+};
+
 const server = createServer((req, res) => {
   logger(req, res, () => {
     jsonMiddleware(req, res, () => {
@@ -61,6 +78,8 @@ const server = createServer((req, res) => {
         req.method === "GET"
       ) {
         getUserByIdHandler(req, res);
+      } else if (req.url === "/api/users" && req.method === "POST") {
+        createUserHandler(req, res);
       } else {
         notFoundHandler(req, res);
       }
