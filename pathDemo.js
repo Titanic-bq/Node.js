e00a25133ed8ed7a91ebe41966a98fbc11145cbb
hendrik <hendrik>
2026-01-19 16:33:34 +0200

    Path moodul

--- pathDemo.js
+++ pathDemo.js
@@ -1,7 +1,18 @@
 import path from "path";
 
 const filepath = "./dir1/dir2/file.txt";
-
+//Täis path
 console.log(path.basename(filepath));
-
+//dir nimi
 console.log(path.dirname(filepath));
+//Faili nimi
+console.log(path.extname(filepath));
+
+//Parsimine
+console.log(path.parse(filepath));
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filepath2 = path.join(__dirname, "dir1", "dir2", "file.html");
+console.log(filepath2);
